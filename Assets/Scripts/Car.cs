df4dffb71dd88ad6e142e589085d088f1c6f4079
Pramish-Aryal <076bct046.pramish@pcampus.edu.np>
2024-01-13 15:19:37 +0545

    overhaul p1

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,48 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    public WheelCollider[] backWheels = new WheelCollider[2];
-    public WheelCollider[] frontWheels = new WheelCollider[2];
+    public float acceleration = 10f;
+    Vector3 velocity = Vector3.zero;
 
-    public float maxAcceleration = 500f;
-    public float breakingForce = 300f;
-    public float maxTurnAngle = 15f;
+    public float forwardsFriction = 1f;
+    public float sidewaysFriction = 10f;
+    public float airResistance = 0.9f;
 
-    float currentAcceleration = 0f;
-    float currentBreakForce = 0f;
-    float currentTurnAngle = 0f;
+    public float angularVelocity = 5f;
+    public float correctionFactor = 0.5f;
+    public float maxTurningSpeed = 200;
 
-    void Start()
+    [SerializeField]
+    float speed;
+
+    Rigidbody rb;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        bool brake = Input.GetKey(KeyCode.Space);
 
-        currentAcceleration = Input.GetAxis("Vertical") * maxAcceleration;
+        // rb.velocity += transform.forward * velocity * vertical * Time.fixedDeltaTime;
+        // rb.velocity = Vector3.RotateTowards(rb.velocity, transform.forward, Vector3.SignedAngle(rb.velocity, transform.forward, transform.up), Time.fixedDeltaTime);
+        // transform.rotation *= Quaternion.FromToRotation(transform.forward, (transform.forward + (transform.right * horizontal) * Time.fixedDeltaTime).normalized);
 
-        currentTurnAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
+        velocity += vertical * transform.forward * acceleration * Time.fixedDeltaTime;
 
-        if (Input.GetKey(KeyCode.Space))
-            currentBreakForce = breakingForce;
-        else
-            currentBreakForce = 0;
+        // friction code starts
+        speed = velocity.magnitude;
 
-        foreach (var wheel in backWheels)
+        float scaleFactorForward = Mathf.Max(0, (speed - forwardsFriction) / speed);
+        float scaleFactorSideways = Mathf.Max(0, (speed - sidewaysFriction) / speed);
+
+        Vector3 forwardsVelocity = Vector3.Project(velocity, transform.forward);
+        Vector3 sidewaysVelocity = velocity - forwardsVelocity;
+
+        forwardsVelocity *= scaleFactorForward;
+        sidewaysVelocity *= scaleFactorSideways;
+
+        velocity = forwardsVelocity + sidewaysVelocity;
+
+        // friction code ends
+
+        // air resistance
+
+        velocity *= 1f / (1 + speed * airResistance / 80000);
+
+
+        if (brake)
         {
-            wheel.motorTorque = currentAcceleration;
+            velocity *= 0.95f;
         }
 
-        foreach (var wheel in frontWheels)
-        {
-            // wheel.motorTorque = currentAcceleration;
-            wheel.steerAngle = currentTurnAngle;
-        }
+        speed = velocity.magnitude;
 
+        Vector3 newPosition = rb.position + velocity * Time.fixedDeltaTime;
+
+        // transform.eulerAngles += new Vector3(Vector3.SignedAngle(transform.up, Vector3.up, transform.forward) * correctionFactor, 0, 0);
+        transform.eulerAngles += new Vector3(0, horizontal * (speed < maxTurningSpeed ? speed : 0) * angularVelocity * Time.fixedDeltaTime, 0);
+        transform.eulerAngles = new Vector3(Mathf.LerpAngle(transform.eulerAngles.x, 0, 0.1f), transform.eulerAngles.y, Mathf.LerpAngle(transform.eulerAngles.z, 0, 0.5f));
+
+
+        rb.MovePosition(newPosition);
+
+        // rb.AddTorque(transform.up * angularVelocity * horizontal);
+        // transform.rotation *= Quaternion.FromToRotation(transform.forward, rb.velocity.normalized);
+        // transform.rotation *= Quaternion.FromToRotation(transform.up, Vector3.up);
     }
-
-
 }
--- Assets/Scripts/FollowerCamera.cs
+++ Assets/Scripts/FollowerCamera.cs
@@ -1,41 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowerCamera : MonoBehaviour
 {
-    public Transform target; // The target object for the camera to follow
-    public float smoothSpeed = 0.125f; // The speed of the camera's movement
-    public Vector3 offset = new Vector3(0, 5, -5); // The offset of the camera from the target
+    public float followDistance;
+    public float followHeight;
+    
+    public float rotationLerpRate;
+    public float heightLerpRate;
 
+    public Transform target;
 
-    public float speed = 5f; // The speed of the sphere
-    public float rotationSpeed = 200f; // The rotation speed of the sphere
+    public bool flipWithMovement = false;
+
+    Vector3 rotation = new Vector3();
+
     void FixedUpdate()
     {
-        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 targetMotionDir = target.InverseTransformDirection(target.GetComponent<Rigidbody>().velocity);
+        rotation.y = target.eulerAngles.y + (targetMotionDir.z > -0.1f || !flipWithMovement ? 0 : 180f); // flip camera if the car goes backwards
 
-        float rotateHorizontal = Input.GetAxis("Horizontal");
+        float cameraAngle = Mathf.LerpAngle(transform.eulerAngles.y, rotation.y, rotationLerpRate * Time.fixedDeltaTime);
+        transform.position = target.position - Quaternion.Euler(0, cameraAngle, 0) * Vector3.forward * followDistance;
 
-        Vector3 force = (Vector3.ProjectOnPlane(transform.forward, Vector3.up)).normalized * moveVertical * speed * Time.fixedDeltaTime;
+        float cameraHeight = Mathf.Lerp(transform.position.y, target.position.y + followHeight, heightLerpRate * Time.fixedDeltaTime);
+        transform.position = new Vector3(transform.position.x, cameraHeight, transform.position.z);
 
-        float turnRotation = rotateHorizontal * rotationSpeed * Time.fixedDeltaTime;
-
-        offset = RotateVectorAboutY(offset, turnRotation);
-        offset.Set(offset.x, Mathf.Max((5 - force.magnitude * 10f), 1f), offset.z);
-
-        Vector3 desiredPosition = target.position + offset;
-
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-
-        transform.LookAt(Vector3.Lerp(transform.position + transform.forward, target.position, smoothSpeed));
-
-    }
-
-    public static Vector3 RotateVectorAboutY(Vector3 vector, float angle)
-    {
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        return rotation * vector;
+        transform.LookAt(target);
     }
 }
